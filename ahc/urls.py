9ca67a42d4a0e3c994da49a55b09f979bf142b3f
alokkjnu <ry7069577@gmail.com>
2022-04-22 03:00:32 +0530

    AHC

---
+++ ahc/urls.py
@@ -0,0 +1,31 @@
+"""ahc URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from ahc_app.views import SignUpView
+from ahc_broker.views import BrokerSignUpView
+from ahc_super_client.views import SuperClientSignUpView
+from ahc_client.views import ClientSignUpView
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('ahc_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/signup/broker/', BrokerSignUpView.as_view(), name='broker_signup'),
+    path('accounts/signup/super_client/', SuperClientSignUpView.as_view(), name='super_client_signup'),
+    path('accounts/signup/client/', ClientSignUpView.as_view(), name='client_signup'),
+    #path('', include('ahc_app.urls', namespace='ahc_app')),
+]
